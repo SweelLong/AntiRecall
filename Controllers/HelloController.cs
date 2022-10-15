515caba378fe5e786665ed2176caae31e54917d3
SweelLong <1485721541@qq.com>
2022-10-15 16:02:52 +0800

    push

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AntiRecall.Controllers
+{
+    [Route("api/plugins/[controller]")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+        public ActionResult Get()
+        {
+            string str = $"Hello AntiRecall ! ";
+            return Ok(str);
+        }
+    }
+}
---
+++ SettingsModel.cs
@@ -0,0 +1,11 @@
+﻿using Konata.Core.Message;
+using PluginCore.Models;
+using System.Collections.Generic;
+
+namespace AntiRecall
+{
+    public class SettingsModel : PluginSettingsModel
+    {
+        public int ClearHistoryByNum { get; set; }
+    }
+}
